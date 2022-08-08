7afa734aa710fc7989ed6d54a6514f987a54bb57
Alexander Kraus <akraus53@gmail.com>
2022-08-08 16:03:38 +0200

    Update

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,15 @@
 (function() {
-  console.log('bookmarklet starting');
-  let paragraphs = document.getElementsByTagName('p');
-  for (let i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].innerHTML = 'puppy';
-  }
+// get all Video links 
+let ls = document.links
+let linklist = [...ls].filter((a) => a.offsetWidth == 170)
+
+// loop over all video links
+for(let link of linklist){
+  open(link);
+  document.getElementsByClassName("vjs-poster")[0].click();
+  console.log(document.getElementsByClassName("vjs-tech")[0].src);
+
+}
+
+
 })();
